76f925d11a8451c428c08d0816f24a504ea795bb
Jinseob Kim <jinseob2kim@gmail.com>
2021-07-25 13:30:02 +0000

    version 1.0.6

--- R/svyCreateTableOneJS.R
+++ R/svyCreateTableOneJS.R
@@ -29,7 +29,8 @@
 #'  nhanesSvy <- svydesign(ids = ~ SDMVPSU, strata = ~ SDMVSTRA, weights = ~ WTMEC2YR, 
 #'                         nest = TRUE, data = nhanes)
 #'  svyCreateTableOne2(vars = c("HI_CHOL","race","agecat","RIAGENDR"), 
-#'                     strata = "RIAGENDR", data = nhanesSvy)
+#'                     strata = "RIAGENDR", data = nhanesSvy,
+#'                     factorVars = c("HI_CHOL", "race", "RIAGENDR"))
 #' @rdname svyCreateTableOne2
 #' @importFrom data.table data.table :=
 #' @importFrom tableone svyCreateTableOne 
@@ -144,7 +145,8 @@ svyCreateTableOne2 <- function(data, strata, vars, factorVars, includeNA = F, te
 #'  nhanesSvy <- svydesign(ids = ~ SDMVPSU, strata = ~ SDMVSTRA, weights = ~ WTMEC2YR, 
 #'                         nest = TRUE, data = nhanes)
 #'  svyCreateTableOneJS(vars = c("HI_CHOL","race","agecat","RIAGENDR"), 
-#'                      strata = "RIAGENDR", data = nhanesSvy)
+#'                      strata = "RIAGENDR", data = nhanesSvy, 
+#'                      factorVars = c("HI_CHOL", "race", "RIAGENDR"))
 #' @rdname svyCreateTableOneJS
 #' @importFrom data.table data.table := CJ
 #' @importFrom tableone svyCreateTableOne 
